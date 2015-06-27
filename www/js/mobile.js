5bcf54e067f53dc8e0262a8d8e8e92e76afee83b
camarin24 <cm961224@gmail.com>
2015-06-26 21:00:16 -0500

    V20
    Estoy conectando con base de datos, por favor no modificar hasta nuevo
    aviso.

--- www/js/mobile.js
+++ www/js/mobile.js
@@ -10,3 +10,29 @@ $("#radio-nat-1").click(function(){
 $("#radio-nat-2").click(function(){
 	$("#jud").fadeIn(500);
 });
+
+var serviceURL = "localhost/servicios/";
+
+var employees;
+
+$('#employeeListPage').bind('pageinit', function(event) {
+	getEmployeeList();
+});
+
+function getEmployeeList() {
+	$.getJSON(serviceURL + 'getemployees.php', function(data) {
+		$('#employeeList li').remove();
+		employees = data.items;
+		$.each(employees, function(index, employee) {
+			$('#employeeList').append('<li class="ui-li-has-alt ui-li-has-thumb ui-last-child"><a href="#" class="ui-btn">
+                    <img src="http://tropi.azurewebsites.net/images/Catalogo/0050438.jpg">j
+                       <h2 id="fullName"></h2>
+                       <p id="alias"></p>
+                       <p id="direccion"></p>
+                  
+                    <a href="#purchase" data-rel="popup" data-position-to="window" data-transition="pop" aria-haspopup="true" aria-owns="purchase" aria-expanded="false" class="ui-btn ui-btn-icon-notext ui-icon-gear" title="Purchase album"></a>
+                </li>');
+		});
+		$('#employeeList').listview('refresh');
+	});
+}
