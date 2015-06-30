c1a21d0a0fbdcb228b4431468a940a18a13eddcd
camarin24 <cm961224@gmail.com>
2015-06-30 09:26:49 -0500

    Conexión con webservice exitosa :)
    Se logro conectar la aplicación con un web service hecho en base a REST,
    se listan los datos de los referidos al momento de entrar al lugar de
    los mismos en la aplicación.

--- www/js/mobile.js
+++ www/js/mobile.js
@@ -11,28 +11,26 @@ $("#radio-nat-2").click(function(){
 	$("#jud").fadeIn(500);
 });
 
-var serviceURL = "localhost/servicios/";
+// var serviceURL = "http://localhost/servicios/";
+// var employees;
 
-var employees;
+// $('#employeeListPage').bind('pageinit', function(event) {
+// 	getEmployeeList();
+// });
 
-$('#employeeListPage').bind('pageinit', function(event) {
-	getEmployeeList();
-});
-
-function getEmployeeList() {
-	$.getJSON(serviceURL + 'getemployees.php', function(data) {
-		$('#employeeList li').remove();
-		employees = data.items;
-		$.each(employees, function(index, employee) {
-			$('#employeeList').append('<li class="ui-li-has-alt ui-li-has-thumb ui-last-child"><a href="#" class="ui-btn">
-                    <img src="http://tropi.azurewebsites.net/images/Catalogo/0050438.jpg">j
-                       <h2 id="fullName"></h2>
-                       <p id="alias"></p>
-                       <p id="direccion"></p>
-                  
-                    <a href="#purchase" data-rel="popup" data-position-to="window" data-transition="pop" aria-haspopup="true" aria-owns="purchase" aria-expanded="false" class="ui-btn ui-btn-icon-notext ui-icon-gear" title="Purchase album"></a>
-                </li>');
-		});
-		$('#employeeList').listview('refresh');
-	});
-}
+// function getEmployeeList() {
+// 	$.getJSON(serviceURL + 'getemployees.php', function(data) {
+// 		$('#employeeList').remove();
+// 		employees = data.items;
+// 		$.each(employees, function(index, employee) {
+// 			$('#employeeList').append('<li class="ui-li-has-alt ui-li-has-thumb ui-last-child"><a href="#" class="ui-btn">'+
+//                     '<img src="http://tropi.azurewebsites.net/images/Catalogo/0050438.jpg">'+
+//                        '<h2>'+ employee.nombre1 +'</h2>'+
+//                        '<p id="alias">'+ employee.nombre2 +'</p>'+
+//                        '<p id="direccion">'+ employee.direccion +'</p>'+
+//                     '<a href="#purchase" data-rel="popup" data-position-to="window" data-transition="pop" aria-haspopup="true" aria-owns="purchase" aria-expanded="false" class="ui-btn ui-btn-icon-notext ui-icon-gear" title="Purchase album"></a>'+
+//                 '</li>');
+// 		});
+// 		$('#employeeList').listview('refresh');
+// 	});
+// }
